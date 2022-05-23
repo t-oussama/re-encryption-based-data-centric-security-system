9b0c8d89ee9dfd53d5ad6c235b33116edddf6a9c
t-oussama <trabelsi.oussama010@gmail.com>
2022-05-24 00:01:20 +0100

    optimize build

--- aont-based-encryption/build.sh
+++ aont-based-encryption/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-g++ -std=gnu++11 -c -fPIC AontBasedEncryption.cpp -o bin/AontBasedEncryption.o
-g++ -std=gnu++11 -shared -Wl,-soname,libAontBasedEncryption.so -o bin/libAontBasedEncryption.so  bin/AontBasedEncryption.o
+g++ -std=gnu++11 -O3 -c -fPIC AontBasedEncryption.cpp -o bin/AontBasedEncryption.o
+g++ -std=gnu++11 -O3 -shared -Wl,-soname,libAontBasedEncryption.so -o bin/libAontBasedEncryption.so  bin/AontBasedEncryption.o
